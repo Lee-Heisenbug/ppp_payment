d7a96c3e900fb0faf286384afce1be004ba96d86
Lee-Heisenbug <1070068427@qq.com>
2019-06-16 22:25:00 +0800

    Add test about AddSalariedEmployee.

---
+++ __test__/AddSalariedEmployee.test.js
@@ -0,0 +1,38 @@
+import AddSalariedEmployee from '../src/AddSalariedEmployee';
+import SalariedClassification from '../src/SalariedClassification';
+import PaymentClassification from '../src/PaymentClassification';
+import PaymentSchedule from '../src/PaymentSchedule';
+import MonthlySchedule from '../src/MonthlySchedule';
+import PaymentMethod from '../src/PaymentMethod';
+import HoldMethod from '../src/HoldMethod';
+
+test( 'test add salaried employee', () => {
+
+    let empid = 1;
+    let t = AddSalariedEmployee( empid, 'Bob', 'Home', 1000.00 );
+    t.execute();
+
+    let e = PayrollDataBase.getEmployee( empid );
+    expect( e.getName() ).toBe( 'Bob' );
+
+    /**@type { PaymentClassification } */
+    let pc = e.getClassification();
+    /**@type { SalariedClassification } */
+    let sc = pc;
+    expect( sc instanceof SalariedClassification ).toBe( true );
+
+    expect( sc.getSalary() ).toBe( 1000.00 );
+
+    /**@type { PaymentSchedule } */
+    let ps = e.getSchedule();
+    /**@type { MonthlySchedule } */
+    let ms = ps;
+    expect( ms instanceof MonthlySchedule ).toBe( true );
+
+    /**@type { PaymentMethod } */
+    let pm = e.getMethod();
+    /**@type { HoldMethod } */
+    let hm = pm;
+    expect( hm instanceof HoldMethod ).toBe( true );
+
+} );
