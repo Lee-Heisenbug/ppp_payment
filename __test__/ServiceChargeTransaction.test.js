7fb0347311bb1f6a810e148899b5be394c984cc2
Lee-Heisenbug <1070068427@qq.com>
2019-07-03 00:29:17 +0800

    Fix test.

--- __test__/ServiceChargeTransaction.test.js
+++ __test__/ServiceChargeTransaction.test.js
@@ -1,12 +1,13 @@
 import ServiceChargeTransaction from '../src/ServiceChargeTransaction';
 import AddHourlyEmployee from '../src/AddHourlyEmployee';
 import PayrollDataBase from '../src/PayrollDataBase';
+import UnionAffiliation from '../src/UnionAffiliation';
 
 test( 'able to add service charge', () => {
 
     let empId = 2;
     let memberId = 86;
-    let date = new Date( 2001, 11, 01 ).getMilliseconds();
+    let date = new Date( 2001, 11, 1 ).getMilliseconds();
 
     let t = new AddHourlyEmployee( empId, 'Bill', 'Home', 15.25 );
     t.execute();
@@ -14,12 +15,13 @@ test( 'able to add service charge', () => {
     let e = PayrollDataBase.getEmployee( empId );
     expect( e ).not.toBe( undefined );
     
-    let af = new UnionAffiliation( memberId, 12.5 );
+    let af = new UnionAffiliation( memberId );
     e.setAffiliation( af );
 
     PayrollDataBase.addUnionMember( memberId, e );
     let sct = new ServiceChargeTransaction( memberId, date, 12.95 );
     sct.execute();
+    /**@type { ServiceCharge } */
     let sc = af.getServiceCharge( date );
     expect( sc.getAmount() ).toBe( 12.95 );
 
